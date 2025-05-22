9cda717cd4beec798a19101b5ba8f82c7c1456f0
simonLoutfi <simon.loutfi@hotmail.com>
2025-05-22 11:08:12 +0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import React from 'react';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<Login />} /> 
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
